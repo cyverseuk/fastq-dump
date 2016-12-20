e21a19f0665f9397e5000cd60f35f7386be5bee3
teatree1212 <Annemarie.Eckes@earlham.ac.uk>
2016-12-20 15:08:44 +0000

    specify pathname for Seq_names.txt

--- runFastqDump.sh
+++ runFastqDump.sh
@@ -4,16 +4,13 @@
 echo "creating output dir"
 mkdir SRA_download
 
-SEQ_LIST=$1
-mv $SEQ_LIST tmp
-echo "moving $SEQ_LIST to tmp"
-
+cp ../Seq_names.txt .
 
 while read line;
 do
 #retrieve sequences
 echo "retrieving $line from SRA"
 fastq-dump $line --outdir SRA_download;
-done < tmp/$SEQ_LIST
+done < tmp/Seq_names.txt
 
 #-v "$(pwd)":/data -w /data
