725396980d051e1cddda3d383ffe374f4e2351f8
teatree1212 <Annemarie.Eckes@earlham.ac.uk>
2016-12-20 09:50:26 +0000

    change docker image name

--- agave/wrapper.sh
+++ agave/wrapper.sh
@@ -4,15 +4,15 @@
 INPUT_1="${input_1}"
 #ADAPTERS="${adapters}"
 
-cp lib/templatesubmit.htc lib/condorsubmit.htc
-echo transfer_input_files = $INPUT_1 >> sratools_submit.htc
-echo arguments = prefetch $INPUT_1 >> sratools_submit.htc
+cp fastq_dump_submit.htc condorsubmit.htc
+echo transfer_input_files = $INPUT_1 >> fastq_dump_submit.htc
+echo arguments = prefetch $INPUT_1 >> fastq_dump_submit.htc
 
 #H5COMMA=`echo ${h5} | sed -e 's/ /,/g'`
 #echo transfer_input_files = ${adapters},$H5COMMA >> sratools_submit.htc
-echo queue >> sratools_submit.htc
+echo queue >> fastq_dump_submit.htc
 
-jobid=`condor_submit sratools_submit.htc`
+jobid=`condor_submit fastq_dump_submit.htc`
 jobid=`echo $jobid | sed -e 's/Sub.*uster //'`
 jobid=`echo $jobid | sed -e 's/\.//'`
 
