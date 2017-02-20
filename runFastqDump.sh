0365ad3bf5b673ba330b94b39a0381ea9d9ca305
teatree1212 <Annemarie.Eckes@earlham.ac.uk>
2017-02-20 17:25:36 +0000

    remove basename

--- runFastqDump.sh
+++ runFastqDump.sh
@@ -4,12 +4,12 @@ ls
 echo "creating output dir"
 mkdir SRA_download
 cd SRA_download
-cp $1 .
+cp ../$1 .
 
 while read line
 ##retrieve sequences
 do echo "retrieving $line from SRA"
 fastq-dump $line  --outdir .
-done <`basename $1`
+done <$1
 
 #-v "$(pwd)":/data -w /data
