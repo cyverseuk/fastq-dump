d3a471457b982e53e95756404490e3d289eaf7ae
teatree1212 <Annemarie.Eckes@earlham.ac.uk>
2017-02-20 17:00:19 +0000

    update wrapper

--- agave/wrapper.sh
+++ agave/wrapper.sh
@@ -12,14 +12,14 @@ echo $filename
 
 
 cp fastq_dump_submit.htc condorsubmit.htc
-echo transfer_input_files = $Seq_names >> fastq_dump_submit.htc
-echo arguments = $filename  >> fastq_dump_submit.htc
+echo transfer_input_files = $Seq_names >> condorsubmit.htc
+echo arguments = $filename  >> condorsubmit.htc
 
 #H5COMMA=`echo ${h5} | sed -e 's/ /,/g'`
 #echo transfer_input_files = ${adapters},$H5COMMA >> sratools_submit.htc
-echo queue >> fastq_dump_submit.htc
+echo queue >> condorsubmit.htc
 
-jobid=`condor_submit fastq_dump_submit.htc`
+jobid=`condor_submit condorsubmit.htc`
 jobid=`echo $jobid | sed -e 's/Sub.*uster //'`
 jobid=`echo $jobid | sed -e 's/\.//'`
 
